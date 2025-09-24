d93210c5c102c2739dd8c35cf574744c4000e78d
cbrimmage1 <chrissybrimmage@gmail.com>
2025-09-24 13:03:47 -0400

    Added CSS styling

---
+++ app.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', function () {
+    console.log('page is loaded');
+
+
+    // ~ GENERATE A PITCH ~ //
+
+    // grab generate button html
+    let button = document.getElementById('generate__button');
+
+    // add click event to button
+    button.addEventListener("click", function () {
+        // testing button works
+        console.log("button was clicked");
+
+        // ~ FETCH API ~ //
+        // when button is clicked, reponse is sought out from API
+        fetch("https://corsproxy.io/?https://itsthisforthat.com/api.php?json")
+            .then(response => response.json())
+            .then(data => {
+                // see the retrieved data in console
+                console.log(data);
+
+                // // grab HTML element where pitch will be displayed on screen
+                let displayedPitch = document.getElementById('displayed__pitch');
+
+                // update it's text content with API data
+                displayedPitch.textContent = `${data.this}` + " for " + `${data.that}`;
+
+                // show the text on screen
+                displayedPitch.style.visibility = 'visible';
+
+
+            })
+
+
+    })
+
+
+
+})
