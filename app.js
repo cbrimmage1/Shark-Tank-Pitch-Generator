72f77063895649fee5221a115e3b699b008bc9c0
cbrimmage1 <chrissybrimmage@gmail.com>
2025-09-30 17:56:35 -0400

    Installed Vite & Three.Js. Added canvas & object

--- app.js
+++ app.js
@@ -1,6 +1,83 @@
+import * as THREE from 'three';
+
+
 window.addEventListener('load', function () {
     console.log('page is loaded');
 
+    // ~ LOAD THREE.JS ELEMENTS ~ //
+
+    // canvas
+    let canvas = document.getElementById('canvas');
+
+    // scene
+    let scene = new THREE.Scene();
+
+    // object
+    let geometry = new THREE.BoxGeometry(1, 1, 1);
+    let material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    let box = new THREE.Mesh(geometry, material);
+    scene.add(box);
+
+    // size 
+    let sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
+
+    // update sizes
+    window.addEventListener('resize', function() {
+        
+        // update window sizes
+        sizes.width = window.innerWidth;
+        sizes.height = window.innerHeight;
+
+        // update camera
+        camera.aspect = sizes.width / sizes.height;
+        camera.updateProjectionMatrix();
+
+        // update renderer
+        renderer.setSize(sizes.width, sizes.height);
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    })
+
+    // camera
+    let camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+    // move camera backwards
+    camera.position.z = 3;
+    scene.add(camera);
+
+    // renderer
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+        alpha: true
+    })
+    renderer.setSize(sizes.width, sizes.height);
+    // call renderer to display scene
+    // renderer.render(scene, camera);
+
+    /// ~ ANIMATE ~ //
+    // clock
+    let clock = new THREE.Clock();
+
+    function tick () {
+       
+        // get elapsed time
+        let elapsedTime = clock.getElapsedTime();
+
+        // update box position (move in circle)
+        box.position.y = Math.sin(elapsedTime);
+        box.position.x = Math.cos(elapsedTime);
+
+         // call renderer
+        renderer.render(scene, camera);
+
+        // execute function each drame
+        window.requestAnimationFrame(tick);
+
+
+    }
+
+    tick();
 
     // ~ GENERATE A PITCH ~ //
 
