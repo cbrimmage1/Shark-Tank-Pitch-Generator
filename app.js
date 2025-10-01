5a58050f8bcb26f77314dba85c7365a490e6889e
cbrimmage1 <chrissybrimmage@gmail.com>
2025-10-01 19:51:37 -0400

    Imported GLSL, created water shader, style updates

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 
-
 window.addEventListener('load', function () {
     console.log('page is loaded');
 
@@ -12,19 +11,202 @@ window.addEventListener('load', function () {
     // scene
     let scene = new THREE.Scene();
 
-    // object
+    // water debug color objects
+    let debugObject = {};
+
+    // test geometry & material
     let geometry = new THREE.BoxGeometry(1, 1, 1);
     let material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    let box = new THREE.Mesh(geometry, material);
-    scene.add(box);
 
-    // size 
+    // test mesh 1
+    let mesh1 = new THREE.Mesh(geometry,material);
+    mesh1.position.set(0, 0.5, -2)
+
+    // test mesh 2
+    let mesh2 = new THREE.Mesh(geometry,material);
+    mesh2.position.set(-2.6, -1, -0.15);
+
+    // test mesh 3
+    let mesh3 = new THREE.Mesh(geometry,material);
+    mesh3.position.set(2, -1, -0.15)
+
+    // create object group to animate in tandem
+    let group = new THREE.Group();
+    group.add(mesh1);
+    group.add(mesh2);
+    group.add(mesh3);
+
+    // add mesh group to three.js scene
+    scene.add(group);
+
+    // ~ CREATE WATER ~ //
+    // water geometry
+    let waterGeometry = new THREE.PlaneGeometry(14,6,256,256);
+
+    // water colors
+    debugObject.depthColor = '#186691'
+    debugObject.surfaceColor = '#9bd8ff'
+
+    // // water material shader
+    let waterMaterial = new THREE.ShaderMaterial({
+        vertexShader: `
+        uniform float uTime;
+        uniform float uBigWavesElevation;
+        uniform vec2 uBigWavesFrequency;
+        uniform float uBigWavesSpeed;
+
+        varying float vElevation;
+
+        // Classic Perlin 3D Noise by Stefan Gustavson
+        vec4 permute(vec4 x)
+        {
+            return mod(((x*34.0)+1.0)*x, 289.0);
+        }
+        vec4 taylorInvSqrt(vec4 r)
+        {
+            return 1.79284291400159 - 0.85373472095314 * r;
+        }
+        vec3 fade(vec3 t)
+        {
+            return t*t*t*(t*(t*6.0-15.0)+10.0);
+        }
+
+        float cnoise(vec3 P)
+        {
+            vec3 Pi0 = floor(P); // Integer part for indexing
+            vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+            Pi0 = mod(Pi0, 289.0);
+            Pi1 = mod(Pi1, 289.0);
+            vec3 Pf0 = fract(P); // Fractional part for interpolation
+            vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+            vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+            vec4 iy = vec4(Pi0.yy, Pi1.yy);
+            vec4 iz0 = Pi0.zzzz;
+            vec4 iz1 = Pi1.zzzz;
+
+            vec4 ixy = permute(permute(ix) + iy);
+            vec4 ixy0 = permute(ixy + iz0);
+            vec4 ixy1 = permute(ixy + iz1);
+
+            vec4 gx0 = ixy0 / 7.0;
+            vec4 gy0 = fract(floor(gx0) / 7.0) - 0.5;
+            gx0 = fract(gx0);
+            vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+            vec4 sz0 = step(gz0, vec4(0.0));
+            gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+            gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+            vec4 gx1 = ixy1 / 7.0;
+            vec4 gy1 = fract(floor(gx1) / 7.0) - 0.5;
+            gx1 = fract(gx1);
+            vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+            vec4 sz1 = step(gz1, vec4(0.0));
+            gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+            gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+            vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+            vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+            vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+            vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+            vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+            vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+            vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+            vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+            vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+            g000 *= norm0.x;
+            g010 *= norm0.y;
+            g100 *= norm0.z;
+            g110 *= norm0.w;
+            vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+            g001 *= norm1.x;
+            g011 *= norm1.y;
+            g101 *= norm1.z;
+            g111 *= norm1.w;
+
+            float n000 = dot(g000, Pf0);
+            float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+            float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+            float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+            float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+            float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+            float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+            float n111 = dot(g111, Pf1);
+
+            vec3 fade_xyz = fade(Pf0);
+            vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+            vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+            float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+            return 2.2 * n_xyz;
+        }
+
+        void main()
+        {
+            vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+            // elevation
+            float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
+                              sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
+                              uBigWavesElevation;
+
+            elevation -= abs(cnoise(vec3(modelPosition.xz * 3.0, uTime * 0.2)) * 0.15);
+
+            modelPosition.y += elevation;
+
+            vec4 viewPosition = viewMatrix * modelPosition;
+            vec4 projectedPosition = projectionMatrix * viewPosition;
+            gl_Position = projectedPosition;
+
+            // varryings
+            vElevation = elevation;
+        }
+        `,
+        fragmentShader: `
+        uniform vec3 uDepthColor;
+        uniform vec3 uSurfaceColor;
+        uniform float uColorOffset;
+        uniform float uColorMultiplier;
+
+        varying float vElevation;
+        
+        void main()
+        {
+            float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+            vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
+            
+            gl_FragColor = vec4(color, 1.0);
+
+            #include <colorspace_fragment>
+        }
+        `,
+        uniforms: 
+        {
+            uTime: { value: 0 },
+            uBigWavesElevation: { value: 0.2 },
+            uBigWavesFrequency: { value: new THREE.Vector2(3, 1.5) },
+            uBigWavesSpeed: { value: 0.75 },
+
+            uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+            uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+            uColorOffset: { value: 0.15 },
+            uColorMultiplier: { value: 2 }
+        }
+        
+    });
+
+    // water mesh
+    let water = new THREE.Mesh(waterGeometry,waterMaterial);
+    water.rotation.x = -1;
+    scene.add(water);
+
+
+    // define window size
     let sizes = {
         width: window.innerWidth,
         height: window.innerHeight
     }
 
-    // update sizes
+    // update window size with resize event
     window.addEventListener('resize', function() {
         
         // update window sizes
@@ -43,7 +225,7 @@ window.addEventListener('load', function () {
     // camera
     let camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
     // move camera backwards
-    camera.position.z = 3;
+    camera.position.z = 3.5;
     scene.add(camera);
 
     // renderer
@@ -52,10 +234,9 @@ window.addEventListener('load', function () {
         alpha: true
     })
     renderer.setSize(sizes.width, sizes.height);
-    // call renderer to display scene
-    // renderer.render(scene, camera);
 
-    /// ~ ANIMATE ~ //
+
+    /// ~ ANIMATE THREE.JS ELEMENTS ~ //
     // clock
     let clock = new THREE.Clock();
 
@@ -64,19 +245,23 @@ window.addEventListener('load', function () {
         // get elapsed time
         let elapsedTime = clock.getElapsedTime();
 
+        // update water
+        waterMaterial.uniforms.uTime.value = elapsedTime;
+
         // update box position (move in circle)
-        box.position.y = Math.sin(elapsedTime);
-        box.position.x = Math.cos(elapsedTime);
+        group.position.x = Math.sin(elapsedTime) * 2;
+        group.position.z = Math.cos(elapsedTime);
 
          // call renderer
         renderer.render(scene, camera);
 
-        // execute function each drame
+        // execute function each frame
         window.requestAnimationFrame(tick);
 
 
     }
 
+    // tick function call
     tick();
 
     // ~ GENERATE A PITCH ~ //
@@ -106,6 +291,15 @@ window.addEventListener('load', function () {
                 // show the text on screen
                 displayedPitch.style.visibility = 'visible';
 
+                // fade in text
+                // referenced from https://www.reddit.com/r/learnjavascript/comments/winvtd/how_do_i_create_a_fade_in_affect_for_dynamically/
+                displayedPitch.animate([{
+                    opacity: 0
+                  }, {
+                    opacity: 1
+                  }], {
+                    duration: 1300
+                  });
 
             })
 
@@ -114,17 +308,30 @@ window.addEventListener('load', function () {
 
 
     // ~ SCROLL PAGE ~ //
-    // grab scroll icon
-    let scrollPage = document.getElementById('scroll__page');
+    // grab scroll icons
+    let scrollPageDown = document.getElementById('scroll__page__down');
+    let scrollPageUp = document.getElementById('scroll__page__up');
 
     // grab 'main' page
-    let scrollHere = document.getElementById('main');
+    let scrollMain = document.getElementById('main');
 
-    // when icon is clicked, scroll to 'main' page
-    scrollPage.addEventListener("click", function() {
-        scrollHere.scrollIntoView({
+    // grab 'landing' page
+    let scrollLanding = document.getElementById('landing');
+
+
+    // when down icon is clicked, scroll to 'main' page
+    scrollPageDown.addEventListener("click", function() {
+        scrollMain.scrollIntoView({
             behavior: "smooth",
             block: "end"
+        })
+    })
+
+    // when up is clicked, scroll to 'landing' page
+    scrollPageUp.addEventListener("click", function() {
+        scrollLanding.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
         })
     })
 
