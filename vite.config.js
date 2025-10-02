8e90b746dad32168f9970ffbc2438d9400db0ecd
cbrimmage1 <chrissybrimmage@gmail.com>
2025-10-02 17:52:33 -0400

    fixed publicDir typo in vite.config,js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 export default {
-    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+    publicDir: './static/', // Path from "root" to static assets (files that are served as they are)
     base: process.env.NODE_ENV === 'production' ? '/Shark-Tank-Pitch-Generator/' : '',
 }
